61814f53f9ec9cb3cd08321d9d033fc309a67a5b
viveka87 <viveka87.s@gmail.com>
2026-07-09 17:42:59 +0530

    fix env variables usage

--- db.py
+++ db.py
@@ -12,10 +12,19 @@
 import pymysql
 import os
 
-connection = pymysql.connect(
-    host=os.getenv("mysql.railway.internal"),
-    user=os.getenv("root"),
-    password=os.getenv("bUdioCPHdvgMASYdOQzuStIVqkXRNcMj"),
-    database=os.getenv("railway"),
-    port=int(os.getenv("3306"))
-)
+#connection = pymysql.connect(
+#   host=os.getenv("mysql.railway.internal"),
+#    user=os.getenv("root"),
+#    password=os.getenv("bUdioCPHdvgMASYdOQzuStIVqkXRNcMj"),
+#    database=os.getenv("railway"),
+#    port=int(os.getenv("3306"))
+#)
+def get_db():
+    return pymysql.connect(
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        #port=int(os.getenv("MYSQLPORT"))
+        port=int(os.getenv("MYSQLPORT", 3306))
+    )
