93d1230d837464bcd7d33f37d02ea9960d0edfe5
viveka87 <viveka87.s@gmail.com>
2026-07-09 12:57:06 +0530

    deploy flask app

--- db.py
+++ db.py
@@ -1,10 +1,21 @@
-import mysql.connector
+#import mysql.connector
 
-def get_db():
-    db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="voice_app"
-    )
-    return db
+#def get_db():
+#    db = mysql.connector.connect(
+#        host="localhost",
+#        user="root",
+#        password="",
+#        database="voice_app"
+ #   )
+#    return db
+
+import pymysql
+import os
+
+connection = pymysql.connect(
+    host=os.getenv("mysql.railway.internal"),
+    user=os.getenv("root"),
+    password=os.getenv("bUdioCPHdvgMASYdOQzuStIVqkXRNcMj"),
+    database=os.getenv("railway"),
+    port=int(os.getenv("3306"))
+)
