567dd5677dff359ebbc959adb8f269fa9aa688e7
viveka87 <viveka87.s@gmail.com>
2026-07-09 13:24:34 +0530

    fix deployment

--- app.py
+++ app.py
@@ -508,4 +508,5 @@ threading.Thread(target=run_scheduler, daemon=True).start()
 
 if __name__ == '__main__':
     app.run(debug=True)
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
